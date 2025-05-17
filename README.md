2c3042bf3de4f3d9e568078360dce9cbbd0cc6c6
c.fraunholz <c.fraunholz@sal-a.de>
2025-05-17 17:38:07 +0200

    adjust setup file

--- README.md
+++ README.md
@@ -3,28 +3,32 @@ MAGD
 
 ## Installation
 Add this to your composer.json:
-  "scripts": {
-    "post-install-cmd": [
-      "php vendor/php10-de/magd/src/scripts/setup.php"
-    ],
-    "post-update-cmd": [
-      "php vendor/php10-de/magd/src/scripts/setup.php"
-    ]
-  },
-  "repositories": [
+
+    "scripts": 
+    {
+      "post-install-cmd": [  
+        "php vendor/php10-de/magd/src/scripts/setup.php"
+      ],
+      "post-update-cmd": [
+        "php vendor/php10-de/magd/src/scripts/setup.php"
+      ]
+    },
+    "repositories": [
     {
       "type": "vcs",
       "url": "https://github.com/php10-de/magd"
     },
-    {
-      "type": "vcs",
-      "url": "https://github.com/php10-de/bigredbutton"
-    }
     ],
-    "require": {
-    "php": ">=7.3",
-    "php10-de/magd": "^1.0"
-  },`
+    "require": 
+    {
+      "php": ">=7.3",
+     "php10-de/magd": "^1.0"
+    },
 
 run
 `composer install`
+
+## New Version
+1. Update the version in composer.json
+2. Add a new tag to the repository, e.g. `git tag -a v1.1.1 -m "Version 1.0.1"`
+3. Push the tag to the repository, e.g. `git push origin v1.1.1`
--- src/scripts/setup.php
+++ src/scripts/setup.php
@@ -1,4 +1,29 @@
 <?php
+use Composer\InstalledVersions;
+
+require_once __DIR__ . '/../../../../../vendor/autoload.php';
+
+$packageName = 'php10-de/magd'; // Replace with your package name
+
+$version = InstalledVersions::getVersion($packageName);
+$prettyVersion = InstalledVersions::getPrettyVersion($packageName);
+
+echo "Version: $version\n";
+echo "Pretty Version: $prettyVersion\n";
+
+$phpVersion = PHP_VERSION;
+list($major, $minor) = explode('.', $phpVersion);
+
+echo "PHP Version: $phpVersion\n";
+
+$json = file_get_contents(__DIR__ . '/../../../../../composer.json');
+$data = json_decode($json, true);
+
+$phpPlatformVersion = isset($data['config']['platform']['php']) ? $data['config']['platform']['php'] : null;
+
+echo "PHP Platform Version: $phpPlatformVersion\n";
+
+$ionPhpVersion = $phpPlatformVersion ? $phpPlatformVersion : $phpVersion;
 
 function recursiveCopy(string $src, string $dst, array $omitDirs = []): void {
     $dir = opendir($src);
@@ -37,8 +62,14 @@ if (!is_dir($sourceDir)) {
     exit(1);
 }
 
-echo "🚀 Copying files from $sourceDir to $targetDir\n";
-recursiveCopy($sourceDir, $targetDir, $omit);
+echo "🚀 Copying assets from $sourceDir to $targetDir\n";
+recursiveCopy($sourceDir . 'assets/', $targetDir, $omit);
+
+echo "🚀 Copying files from version $sourceDir $version to $targetDir\n";
+recursiveCopy($sourceDir . $version . '/', $targetDir, $omit);
+
+echo "🚀 Copying ioncube  files from $sourceDir to $targetDir\n";
+recursiveCopy($sourceDir . $ionPhpVersion . '/', $targetDir, $omit);
 
 $dstPath = $targetDir . '/../docker-compose.example.yml';
 copy($sourceDir . '../docker-compose.yml', $targetDir . '/../docker-compose.dist.yml');
